da7e6c34f4ae037d508ebdb734e7a718bf07f479
Tu Nombre <tu@email.com>
2026-06-23 12:45:26 -0400

    BERSERKER Tournament Control - Frontend + Backend funcionando

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BerserkerProducerPanel from './components/BerserkerProducerPanel';
+
+function App() {
+  return <BerserkerProducerPanel />;
+}
+
+export default App;
